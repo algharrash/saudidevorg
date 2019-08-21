ef81d76240cb10952c2a7bb601f1cf65c1a97c88
Ali AlGharrash <ali.algarrash@gmail.com>
2019-08-21 23:33:02 +0300

    Day 002 Code

---
+++ day002.py
@@ -0,0 +1,19 @@
+#This is a line of comment
+print("Hello World")
+
+print("مرحبا") #This is inline comment
+
+#This comment
+#is written in
+#multiple line
+print("After multiple line comment")
+
+#print("This code should not be printed")
+
+"""
+Thi is a comment
+written in
+more than multiple line
+"""
+print("After Multiline string")
+
