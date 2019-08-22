0ef0e90480119e47dfa8171f05553c28d308dad8
Ali AlGharrash <ali.algarrash@gmail.com>
2019-08-23 02:43:34 +0300

    Day 003 Code

---
+++ day003.py
@@ -0,0 +1,17 @@
+#Different kind of variable
+Current_Year = 2019
+x, y = 10, 20
+X = Y = 10
+z = x + y
+
+#String Variable
+fname = 'Ali '
+sname = "Ahmad "
+lname = "AlGharrash"
+full_name = fname + sname + lname
+
+#printing the variable
+print(Current_Year)
+print(z)
+print(X + Y)
+print("My Name is: " + full_name)
