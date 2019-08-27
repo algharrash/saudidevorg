54df17c000504eb5a699627785e500c5601c99ac
Ali AlGharrash <ali.algarrash@gmail.com>
2019-08-27 22:20:48 +0300

    Day 008 Code

---
+++ dsy008.py
@@ -0,0 +1,22 @@
+#Python String Methods
+
+x = "   My Name:   "
+my_name = "Ali Ahmad AlGharrash"
+
+#print the length of string
+print(len(my_name))
+
+#remove whitespaces from the string
+print(x.strip())
+
+#convert strin to uppercase
+print(my_name.upper())
+
+#convert string to lowercase
+print(my_name.lower())
+
+#split the string
+print(my_name.split(" "))
+
+#replace all " " with "," in string
+print(my_name.replace(" ", ","))
