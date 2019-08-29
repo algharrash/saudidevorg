59b75f15a0f0dc28fa2623ca40de838294a7b728
Ali AlGharrash <ali.algarrash@gmail.com>
2019-08-30 01:41:27 +0300

    Day 010 Code

---
+++ day010.py
@@ -0,0 +1,15 @@
+#python operater part 1
+
+#assignment operator
+x = 5
+y = 10
+
+#arthematic operator
+print (x + y)
+print (x//y)
+print (x % y)
+
+#logical operator
+print (x == y)
+print (x != y)
+print (x > y)
