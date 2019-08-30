d2cea7d877f5148e7047939fcdcc87c175d79018
Ali AlGharrash <ali.algarrash@gmail.com>
2019-08-31 00:44:48 +0300

    Day 011 Code

---
+++ day011.py
@@ -0,0 +1,23 @@
+#Python operators 2
+
+#logical operaters
+x = 2
+y = 10
+
+print (x > 2 and y <5)
+print (x == 2 or y < 5)
+print (not(x > 2 or y < 5))
+
+#Identity Opertator
+m1 = [1, 2, 3]
+m2 = [1, 2, 3]
+m3 = m1
+
+print (m1 is m2)
+print (m1 is m3)
+
+#Membership operator
+
+print (x in m1)
+print (y not in m1)
+
