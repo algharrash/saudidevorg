2bcef01a6576d8d64cd9a79e65153a21ed2ffb75
Ali AlGharrash <ali.algarrash@gmail.com>
2019-09-01 21:08:41 +0300

    Day 013 Code

---
+++ day013.py
@@ -0,0 +1,19 @@
+#Python List
+
+x = [10, 20, 30, 40]
+
+print("Print th x list:")
+print(x)
+
+print("Print the items of x list:")
+for n in x:
+    print(n)
+
+del x[2]
+
+print("Print x list after deleting item number (3):")
+for n in x:
+    print(n)
+
+
+
