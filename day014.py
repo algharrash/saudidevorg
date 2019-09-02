c1c1af3c77ac1c956417fd5a1b2b0b11722190f5
Ali AlGharrash <ali.algarrash@gmail.com>
2019-09-02 23:18:52 +0300

    Day 014 Code

---
+++ day014.py
@@ -0,0 +1,24 @@
+#python list part 2
+
+m = [1, 2, 3 , 4, 5, 6 , 7]
+
+print ("print the m list")
+print (m)
+
+#print a range of items in the list
+print("print items from 2 - 5")
+print (m[3:6])
+
+#search about item in the list
+print ("search if 7 is in the m list")
+if 7 in m:
+    print("7 is in the list")
+
+#add two lists
+n = [12.5, 2.5]
+
+print("print the n list")
+print(n)
+print("adding m and n lists")
+print(m + n)
+
