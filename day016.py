4e2f08968de3f4dd37d70b586ef917f35d1619b6
Ali AlGharrash <ali.algarrash@gmail.com>
2019-09-04 22:10:20 +0300

    Day 016 Code

---
+++ day016.py
@@ -0,0 +1,14 @@
+#Python tuples part 1
+
+#creating mytuple and primt it
+mytuple = ('Ali', 'Khadija', 'Ahmad', 'Fatimah')
+print(mytuple)
+
+print("Print all the items in mytuple")
+for x in mytuple:
+    print(x)
+
+print("Separate mytuple and print the first two items")
+print(mytuple[0:2])
+
+
