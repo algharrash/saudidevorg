3ba23153255277d97d0f5d2b467834679d5476a8
Ali AlGharrash <ali.algarrash@gmail.com>
2019-09-09 00:05:18 +0300

    Day 020 Code

---
+++ day020.py
@@ -0,0 +1,11 @@
+#pyhton sets part 1
+
+OfficeTools = {'Pen', 'Sticky Note'}
+OfficeTools.add('Calendar')
+OfficeTools.update(['Folders', 'Printer', 'PC'])
+
+print("My office tools are:")
+for x in OfficeTools:
+    print(x)
+
+
