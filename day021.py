885b53e471be041ce2be6a82b2b1d170a2e4547a
Ali AlGharrash <ali.algarrash@gmail.com>
2019-09-10 00:05:13 +0300

    Day 021 Code

---
+++ day021.py
@@ -0,0 +1,9 @@
+#Python set part 2
+
+ShoppingList = set(('Meet', 'Cheese', 'Bread', 'Milk'))
+print("The count of items in my shopping list are:")
+print(len(ShoppingList))
+
+ShoppingList.discard("Milk")
+print(ShoppingList)
+
