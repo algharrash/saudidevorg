4b79a22026a87a67bbdd2c7fa47c726619f68335
Ali AlGharrash <ali.algarrash@gmail.com>
2019-09-28 22:45:58 +0300

    Day 037 & 038 Code

---
+++ day037_038.py
@@ -0,0 +1,19 @@
+#Arrays in python
+
+shoppingList = ['Milk', 'Bread']
+
+print("The shopping list contains:", shoppingList)
+
+shoppingList.append('Meat')
+shoppingList.append('Chicken')
+shoppingList.append('Snacks')
+shoppingList.append('Rice')
+shoppingList.append('Eggs')
+print("The shopping list after appnding some items:", shoppingList)
+
+shoppingList.reverse()
+print("The shopping list after reversing its items:", shoppingList)
+
+shoppingList.sort()
+print("The shopping list after sorting its items:", shoppingList)
+
